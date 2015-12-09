6158640b59c1d133701f9b335e074cde075b52e1
Zoe <doticus@gmail.com>
2015-12-08 22:46:49 -0500

    Added topics show page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,1 +1,7 @@
 class TopicsController < ApplicationController
+
+  def show
+    #@book = Book.find(params[:book_id])
+    @topic = Topic.find(params[:id])
+  end
+end
---
+++ spec/features/user_visits_topic_page_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+feature "User visits topic page" do
+#    %q(
+#   As a user
+#   I want to view topics
+#   So that I can see what others are talking about
+#
+#   Acceptance Criteria
+#
+#   [X] I must see the topics for each book
+# )
+
+  scenario "User visits topic page" do
+
+    book = FactoryGirl.create(:book)
+    topic = FactoryGirl.create(:topic, book: book)
+
+    visit "books/#{book.id}"
+    click_on topic.title
+
+    expect(page).to have_content topic.title
+    end
+  end
+
+#   scenario "Most recent posted first" do
+#
+#     book = FactoryGirl.create(:book)
+#     second_book = FactoryGirl.create(:book)
+#
+#     visit root_path
+#     (second_book.title).should appear_before(book.title)
+#
+#   end
+# end
